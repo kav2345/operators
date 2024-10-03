b0aed4d89898ca2f17f40476ab2a7959de94f87f
kav2345 <22hr1a3250@mtieat.org>
2024-10-03 01:32:29 -0700

    Update slope.py

--- slope.py
+++ slope.py
@@ -1,3 +1,4 @@
+***
  Finding the Slope of a Line
 Problem Statement
 Ajay and Binoy are curious about the slope of the line joining their houses. Given the coordinates of the 2 endpoints of a line (x1,y1)(x_1, y_1)(x1​,y1​) and (x2,y2)(x_2, y_2)(x2​,y2​), write a Python program to find the slope of the line.
@@ -17,9 +18,7 @@ Sample Input
 Sample Output
 
 The slope of the line joining Ajay's house and Binoy's house is 2.00
-
-Solution
-
+***
 x1 = int(input())
 y1 = int(input())
 x2 = int(input())
@@ -30,4 +29,3 @@ if x2 != x1:
     print(f"The slope of the line joining Ajay's house and Binoy's house is {slope:.2f}")
 else:
     print("The line is vertical, slope is undefined")
-
